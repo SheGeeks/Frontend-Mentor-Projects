25d5ae1df1666cdabeffa9f650dd43e8521ffea7
Corvida Raven <dev@shegeeks.net>
2021-09-07 23:37:57 -0400

    Add event listeners, functions and error message

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,18 +1,25 @@
-const container = document.querySelector(".percentages");
-let percentages;
-const percent = document.querySelectorAll(".percent.selected");
-let billInput, partyCount, tipTotal, selectedTip, customTip, billTotal;
-// let partyCount;
-// let tipTotal;
-// let selectedTip;
-// let customTip;
-// let billTotal;
+"use strict";
 
-//Functions
+const container = document.querySelector(".percentages");
+const percent = document.querySelectorAll(".percent.selected");
+const billInput = document.querySelector("#bill");
+const partyInput = document.querySelector("#party-count");
+const resetBtn = document.querySelector("#reset");
+const customTipInput = document.querySelector("#custom-tip");
+const err = document.querySelector(".err");
+let billAmount,
+  partyCount,
+  tipTotal,
+  selectedTip,
+  customTip,
+  billTotal,
+  percentages;
+
+////// Functions
 // Calculate Tip Per Person
 const calcTip = function (percent) {
   tipTotal = document.getElementById("tip-total").textContent = (
-    (billInput / partyCount) *
+    (billAmount / partyCount) *
     (percent / 100)
   )
     .toFixed(3)
@@ -20,59 +27,89 @@ const calcTip = function (percent) {
 };
 
 // Calculate Total Per Person
-const calcTotal = function () {
+const calcBill = function () {
   billTotal = document.getElementById("bill-total").textContent = (
-    billInput / partyCount +
+    billAmount / partyCount +
     Number(tipTotal)
   )
     .toFixed(3)
     .slice(0, -1);
 };
 
-container.addEventListener("click", (e) => {
-  billInput = +document.querySelector("#bill").value;
+//Calculate Tip & Total Per Person
+const calcTotals = function () {
+  billAmount = +document.querySelector("#bill").value;
   partyCount = +document.querySelector("#party-count").value;
-  console.log(percentages);
-  percentages = document.querySelector(".percent.selected");
 
-  // Selected Percent Toggle
-  if (
-    e.target.classList.contains("percent") &&
-    !e.target.classList.contains("custom")
-  ) {
-    if (percentages) {
-      percentages.classList.remove("selected");
-    }
-    e.target.classList.toggle("selected");
-    selectedTip = e.target.getAttribute("percentage");
-    console.log(`Selected Tip: ${selectedTip}%`);
-  } else if (
-    e.target === document.getElementById("custom-tip") ||
-    e.target.classList.contains("custom")
-  ) {
-    if (percentages) {
-      percentages.classList.remove("selected");
-    }
-
-    selectedTip = customTip = +document.getElementById("custom-tip").value;
-
-    document.querySelector(".percent.custom").classList.toggle("selected");
-    document
-      .querySelector(".percent.custom")
-      .setAttribute("percentage", customTip / 100);
-
-    console.log("Custom Tip: ", customTip);
+  if (billAmount && partyCount && selectedTip) {
+    calcTip(selectedTip);
+    calcBill(Number(tipTotal));
+    //Check for empty inputs
+  } else if (!billAmount) {
+    resetTotals();
+  } else if (!selectedTip) {
+    resetTotals();
+  } else {
+    resetTotals();
   }
+};
 
-  //Update Values
-  calcTip(selectedTip);
-  calcTotal(Number(tipTotal));
+// Reset Totals
+const resetTotals = function () {
+  document.getElementById("tip-total").textContent = "0.00";
+  document.getElementById("bill-total").textContent = "0.00";
+};
 
-  // Logs
-  console.log("Total Bill: $" + billInput);
-  console.log("Party of: " + partyCount);
-  console.log("---------");
-  console.log("Tip Per Person: $" + tipTotal);
-  console.log("Total Per Person: $" + billTotal);
-  console.log("--------------------------");
+// Reset Everything
+resetBtn.addEventListener("click", (e) => {
+  document.getElementById("bill").value = "";
+  document.getElementById("party-count").value = "";
+  document.querySelector(".percent.selected").classList.remove("selected");
+  resetTotals();
+});
+
+//// Event Listeners
+// Listen for billinPut
+billInput.addEventListener("keyup", (e) => calcTotals());
+
+//Listen for custom tip
+customTipInput.addEventListener("keyup", (e) => {
+  selectedTip = customTip = +document.getElementById("custom-tip").value;
+  calcTotals();
+});
+
+// Listen for party input
+partyInput.addEventListener("keyup", (e) => {
+  if (partyInput.value > 0 || partyInput.value === "") {
+    err.style.display = "none";
+    calcTotals();
+  } else {
+    err.style.display = "inline";
+  }
+});
+
+// Toggle Tip Percentage
+container.addEventListener("click", (e) => {
+  percentages = document.querySelector(".percent.selected");
+  // Selected Tip Toggle
+  if (
+    e.target.classList.contains("percent") ||
+    e.target === document.getElementById("custom-tip")
+  ) {
+    if (percentages) percentages.classList.remove("selected");
+    e.target.classList.toggle("selected");
+    selectedTip = !e.target.classList.contains("custom")
+      ? e.target.getAttribute("percentage")
+      : (customTip = +document.getElementById("custom-tip").value);
+
+    //select custom tip from input as selected tip
+    if (e.target === document.getElementById("custom-tip")) {
+      selectedTip = customTip = +document.getElementById("custom-tip").value;
+      document.querySelector(".percent.custom").classList.toggle("selected");
+      document
+        .querySelector(".percent.custom")
+        .setAttribute("percentage", customTip / 100);
+    }
+  }
+  calcTotals();
 });

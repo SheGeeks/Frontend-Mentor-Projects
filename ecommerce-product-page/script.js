1cbceebde9f81a6500c6da0b8b398c30d69480c6
Corvida Raven <dev@shegeeks.net>
2022-01-24 00:14:43 -0500

    Add eventListeners to mobile nav and quantity btns

--- ecommerce-product-page/script.js
+++ ecommerce-product-page/script.js
@@ -1,11 +1,39 @@
-mobileNav = document.querySelector(".mobile-nav");
-mobileMenuCloseBtn = document.querySelector("button.mobile-menu-close-btn");
-mobileMenuBtn = document.querySelector(".mobile-menu-open-btn");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileMenuCloseBtn = document.querySelector("#mobile-menu-close-btn");
+const mobileMenuBtn = document.querySelector("#mobile-menu-open-btn");
+const addQuantityBtn = document.querySelector("#add-to-cart #qty-add");
+const subQuantityBtn = document.querySelector("#add-to-cart #qty-subtract");
+const qtyInput = document.querySelector("#add-to-cart #qty-total");
+const addToCartBtn = document.querySelector("#addToCart-btn");
+let qtyTotal = 0;
+qtyInput.value = 0;
 
+console.log(qtyInput);
+
+// //////////// Mobile Nav Menu
 mobileMenuBtn.addEventListener("click", (e) => {
+  console.log("clicked");
   mobileNav.classList.add("open");
+  mobileMenuCloseBtn.classList.add("open");
 });
 
 mobileMenuCloseBtn.addEventListener("click", (e) => {
   mobileNav.classList.remove("open");
+  mobileMenuCloseBtn.classList.remove("open");
+});
+
+// ///////////////// Add Quantity
+
+addQuantityBtn.addEventListener("click", (e) => {
+  qtyTotal++;
+  qtyInput.value = qtyTotal;
+});
+
+subQuantityBtn.addEventListener("click", (e) => {
+  if (!qtyInput.value || qtyInput.value == 0) {
+    qtyInput.value = 0;
+  } else {
+    qtyTotal--;
+    qtyInput.value = qtyTotal;
+  }
 });

96d84f3f63d63d84c6e4d1b6c745377e9422b343
Corvida Raven <dev@shegeeks.net>
2022-01-29 16:29:03 -0500

    Add delete tasks and clear completed tasks

--- todo-app/script.js
+++ todo-app/script.js
@@ -5,8 +5,10 @@ const todoTemplate = document.querySelector(
 );
 const checkmark = document.querySelectorAll("input[type=checkbox]");
 const todoText = document.querySelector("#todo-task");
+const deleteTask = document.querySelector(".delete");
+const clearTasks = document.querySelector("#clear");
 const todoCounter = document.querySelector("#todo-count");
-let counterIndex = 4;
+let counterIndex;
 
 function updateCounter() {
   let todos = document.querySelectorAll(".todo-item:not(#todo-item_new)");
@@ -18,36 +20,41 @@ function updateCounter() {
     }
   });
 
-  if (counterIndex > 1 || counterIndex === 0) {
-    todoCounter.textContent = `${counterIndex} items left`;
-  } else {
-    todoCounter.textContent = `${counterIndex} item left`;
-  }
+  counterIndex > 1 || counterIndex === 0
+    ? (todoCounter.textContent = `${counterIndex} items left`)
+    : (todoCounter.textContent = `${counterIndex} item left`);
 }
+
+updateCounter();
 
 // Create new todo
 newTaskInput.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
-    let newTask = newTaskInput.value;
+    // Create new task and add to end of list
+    const clonedToDo = todoTemplate.cloneNode(true);
+    todoListContainer.appendChild(clonedToDo);
 
-    const newToDoItem = todoTemplate.cloneNode(true);
-    document.getElementById("todo-list").appendChild(newToDoItem);
-
-    const newItem =
-      document.getElementById("todo-list").lastChild.childNodes[1]
-        .childNodes[1];
-    newItem.textContent = newTask;
+    // Change task text to user input
+    const newToDo = document.getElementById("todo-list").lastChild;
+    newToDo.childNodes[1].childNodes[1].textContent = newTaskInput.value;
     newTaskInput.value = "";
+
+    // Remove check and strike from cloned task template
+    if (newToDo.classList.contains("strike")) {
+      newToDo.classList.remove("strike");
+      newToDo.querySelector("input[type=checkbox]").checked = false;
+    }
 
     // Update todo counter
     updateCounter();
   }
 });
 
-// Strike Items When Checked
 todoListContainer.addEventListener("click", (e) => {
   //   e.stopPropagation();
+  console.log(e.target);
 
+  // Strike Tasks When Checked
   if (e.target.matches("input[type=checkbox]")) {
     let todoItem = e.target.closest(".todo-item");
     if (todoItem.classList.contains("strike")) {
@@ -55,7 +62,26 @@ todoListContainer.addEventListener("click", (e) => {
     } else {
       todoItem.classList.add("strike");
     }
-    // Update todo counter
-    updateCounter();
   }
+
+  // Delete Tasks
+  if (
+    e.target.matches("button") ||
+    e.target.matches("svg") ||
+    e.target.matches("path")
+  ) {
+    // e.currentTarget.childNodes
+    e.target.closest(".todo-item").remove();
+  }
+
+  // Update todo counter
+  updateCounter();
+});
+
+// Clear completed tasks
+clearTasks.addEventListener("click", (e) => {
+  let tasksComplete = document.querySelectorAll(".todo-item.strike");
+  tasksComplete.forEach((task) => {
+    task.closest(".todo-item").remove();
+  });
 });

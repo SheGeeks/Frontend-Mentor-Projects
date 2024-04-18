48e7c2937cd764b1eae8a28ac6181b86aa44b974
Corvida Raven <dev@shegeeks.net>
2024-04-18 18:20:11 -0400

    Update text color

--- social-links-react/src/components/Footer.tsx
+++ social-links-react/src/components/Footer.tsx
@@ -1,16 +1,18 @@
 export default function Footer() {
   return (
-    <footer className="text-gray-800 dark:text-white">
-      <small className="text-xs">
-        Coded with 💝 by{" "}
-        <a
-          href="https://dev.shegeeks.net"
-          aria-label="Corvida's portfolio (opens in a new window)"
-          className="text-lprimary hover:text-gray-900 dark:text-accent dark:hover:text-accent/70 underline transition"
-        >
-          Corvida
-        </a>
-      </small>
+    <footer className="text-neutral-800 dark:text-white">
+      <p>
+        <small className="text-xs">
+          Coded with 💝 by{" "}
+          <a
+            href="https://dev.shegeeks.net"
+            aria-label="Corvida's portfolio (opens in a new window)"
+            className="text-lprimary hover:text-gray-900 dark:text-accent dark:hover:text-accent/70 underline transition"
+          >
+            Corvida
+          </a>
+        </small>
+      </p>
     </footer>
   );
 }

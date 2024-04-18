937f59a976e180037865ee5bc29f0eaf271a3853
Corvida Raven <dev@shegeeks.net>
2024-04-18 19:57:08 -0400

    Animate toggle on hover

--- social-links-react/src/components/Toggle.tsx
+++ social-links-react/src/components/Toggle.tsx
@@ -22,7 +22,7 @@ export default function Toggle() {
     <button
       aria-labelledby="toggle-label"
       style={style}
-      className="absolute top-0 right-0 m-[-85px] w-[150px] h-[150px] rotate-45 bg-[length:20%] bg-[bottom_10px_left_50%] shadow-[0_0_0_1px_#607326] border-none bg-sky-950 dark:hover:bg-black/30 bg-no-repeat cursor-pointer hover:bg-black/90 dark:bg-black/70 transition"
+      className="absolute top-0 right-0 m-[-85px] w-[150px] h-[150px] rotate-45 bg-[length:25%] dark:bg-[length:30%] bg-[bottom_-12px_left_50%] dark:bg-[bottom_-21px_left_50%] hover:bg-[bottom_2px_left_50%] dark:hover:bg-[bottom_2px_left_50%] shadow-[0_0_0_1px_#607326] border-none bg-sky-950 dark:hover:bg-black/30 bg-no-repeat cursor-pointer hover:bg-black/90 dark:bg-black/70 transition"
       onClick={toggleTheme}
     >
       <span id="toggle-label" hidden>

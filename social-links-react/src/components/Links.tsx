73a700a7ba558275ab1452a72f7e09b78dff83ba
Corvida Raven <dev@shegeeks.net>
2024-04-18 16:04:32 -0400

    Consolidate social links logic

--- social-links-react/src/components/Links.tsx
+++ social-links-react/src/components/Links.tsx
@@ -1,44 +1,18 @@
 import React from "react";
-import Link from "./Link";
-
-const socialLinks = [
-  {
-    linkUrl: "https://github.com/shegeeks",
-    linkName: "Github",
-    linkClass: "git",
-  },
-  {
-    linkUrl: "https://www.frontendmentor.io/profile/SheGeeks",
-    linkName: "Frontend Mentor",
-    linkClass: "fem",
-  },
-  {
-    linkUrl: "https://linkedin.com/in/corvida",
-    linkName: "LinkedIn",
-    linkClass: "lin",
-  },
-  {
-    linkUrl: "https://twitter.com/corvida",
-    linkName: "X (Twitter)",
-    linkClass: "twit",
-  },
-  {
-    linkUrl: "https://instagram.com/birdingwhileblack",
-    linkName: "Instagram",
-    linkClass: "ig",
-  },
-];
+import { socialLinks } from "../data/socialLinks";
 
 export default function Links() {
   return (
     <div className="w-full flex flex-col gap-5 m-4 text-center">
-      {socialLinks.map((link) => (
-        // eslint-disable-next-line react/jsx-key
-        <Link
-          linkUrl={link.linkUrl}
-          linkName={link.linkName}
-          linkClass={link.linkClass}
-        />
+      {socialLinks.map((link, index) => (
+        <a
+          key={index}
+          href={link.url}
+          target="_blank"
+          className={link.class + ` link`}
+        >
+          {link.name}
+        </a>
       ))}
     </div>
   );
---
+++ social-links-react/src/data/socialLinks.tsx
@@ -0,0 +1,27 @@
+export const socialLinks = [
+  {
+    url: "https://github.com/shegeeks",
+    name: "Github",
+    class: "git",
+  },
+  {
+    url: "https://www.frontendmentor.io/profile/SheGeeks",
+    name: "Frontend Mentor",
+    class: "fem",
+  },
+  {
+    url: "https://linkedin.com/in/corvida",
+    name: "LinkedIn",
+    class: "lin",
+  },
+  {
+    url: "https://twitter.com/corvida",
+    name: "X (Twitter)",
+    class: "twit",
+  },
+  {
+    url: "https://instagram.com/birdingwhileblack",
+    name: "Instagram",
+    class: "ig",
+  },
+];

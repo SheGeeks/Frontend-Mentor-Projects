5604ea0e3f9467dd755f265ceca6c6000904b7f5
Corvida Raven <dev@shegeeks.net>
2024-04-18 18:22:06 -0400

    Remove id attribute

--- social-links-react/src/components/Toggle.tsx
+++ social-links-react/src/components/Toggle.tsx
@@ -21,7 +21,6 @@ export default function Toggle() {
   return (
     <button
       aria-labelledby="toggle-label"
-      id="toggle"
       style={style}
       className="absolute top-0 right-0 m-[-85px] w-[150px] h-[150px] rotate-45 bg-[length:20%] bg-[bottom_10px_left_50%] shadow-[0_0_0_1px_#607326] border-none bg-cyan-900 dark:hover:bg-black/30 bg-no-repeat cursor-pointer hover:bg-black/90 dark:bg-black/70 transition"
       onClick={toggleTheme}
